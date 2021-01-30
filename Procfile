e9208cf639eabb4f836c96163a7fc94cfcdd12db
Naacha588 <75855130+Naacha588@users.noreply.github.com>
2021-01-31 02:50:15 +0700

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m NadiasBot
+web: python3 -m NadiasBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE elseNadiasBot
+:: Enables virtual env mode and then starts elseNadiasBot
+env\scripts\activate.bat && py -m elseNadiasBot
