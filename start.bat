06f677d179eed858b91e0f197bbdddab61e2d12f
Naacha588 <75855130+Naacha588@users.noreply.github.com>
2021-04-05 06:12:27 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE elseNadiasBot
-:: Enables virtual env mode and then starts elseNadiasBot
-env\scripts\activate.bat && py -m elseNadiasBot
+:: Enables virtual env mode and then starts NadiasBot
+env\scripts\activate.bat && py -m NadiasBot
