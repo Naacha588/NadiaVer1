cbba32f70c20eb5a515ff358d418df022c07e8c5
Naacha588 <75855130+Naacha588@users.noreply.github.com>
2021-04-05 06:11:41 +0700

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop elseNadiasBot
-net start elseNadiasBot
+net stop NadiasBot
+net start NadiasBot
